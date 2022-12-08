70264e383f5f7642a6b9cd959e2a628cad8bd953
unknown <fredericreact@gmail.com>
2022-12-09 00:39:01 +0800

    handle errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { RouterProvider, createBrowserRouter, createRoutesFromElements,Route } from 'react-router-dom';
-
+import ErrorPage from './pages/Error'
 import BlogLayout from './pages/BlogLayout';
 import BlogPostsPage, {loader as blogPostsLoader} from './pages/BlogPosts';
 import NewPostPage from './pages/NewPost';
@@ -8,11 +8,11 @@ import RootLayout from './pages/RootLayout';
 import WelcomePage from './pages/Welcome';
 
 const router = createBrowserRouter(createRoutesFromElements(
-<Route path="/" element={<RootLayout/>}>
+<Route path="/" element={<RootLayout/>} errorElement={<ErrorPage/>}>
   <Route index element={<WelcomePage />} />
   <Route path="/blog" element={<BlogLayout />}>
     <Route index element={<BlogPostsPage />} loader={blogPostsLoader}/>
-    <Route path=":id" element={<PostDetailPage />} loader ={blogPostLoader}/>
+    <Route path=":id" element={<PostDetailPage />} loader ={blogPostLoader} />
   </Route>
   <Route path="/blog/new" element={<NewPostPage />} />
 </Route>
---
+++ src/pages/Error.jsx
@@ -0,0 +1,16 @@
+import MainNavigation from '../components/MainNavigation';
+
+function ErrorPage() {
+
+
+  return (
+    <>
+      <MainNavigation />
+      <main id="error-content">
+        <h1>An error occurred!</h1>
+      </main>
+    </>
+  );
+}
+
+export default ErrorPage;
